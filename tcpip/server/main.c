8637536a14cc66b2c6a992afc07b98efd0cfc3ab
mikle9997 <mikle9997@mail.ru>
2018-09-21 23:44:54 +0300

    next step of development

--- tcpip/server/main.c
+++ tcpip/server/main.c
@@ -2,6 +2,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <zconf.h>
+
+#define NUMER_OF_SYMOBLS 5
+
+char* readn( int n, int s, int s1)
+{
+    char buf[n];
+    int number_of_entered = 0;
+
+    while (number_of_entered != n) {
+        s1 = accept(s, NULL, NULL);
+
+    }
+
+
+    return buf;
+}
 
 int main( void )
 {
@@ -14,40 +32,26 @@ int main( void )
     local.sin_family = AF_INET;
     local.sin_port = htons( 7500 );
     local.sin_addr.s_addr = htonl( INADDR_ANY );
+
     s = socket( AF_INET, SOCK_STREAM, 0 );
-    if ( s < 0 )
-    {
-        perror( "socket call failed" );
-        exit( 1 );
-    }
+
     rc = bind( s, ( struct sockaddr * )&local, sizeof( local ) );
-    if ( rc < 0 )
-    {
-        perror( "bind call failure" );
-        exit( 1 );
-    }
+
     rc = listen( s, 5 );
-    if ( rc )
-    {
-        perror( "listen call failed" );
-        exit( 1 );
-    }
-    s1 = accept( s, NULL, NULL );
-    if ( s1 < 0 )
-    {
-        perror( "accept call failed" );
-        exit( 1 );
-    }
+
+    s1 = accept(s, NULL, NULL);
+
+    readn(NUMER_OF_SYMOBLS, s, s1);
+
     rc = recv( s1, buf, 1, 0 );
-    if ( rc <= 0 )
-    {
-        perror( "recv call failed" );
-        exit( 1 );
-    }
+
     printf( "%c\n", buf[ 0 ] );
+
     rc = send( s1, "2", 1, 0 );
-    if ( rc <= 0 )
-        perror( "send call failed" );
+
+    shutdown(s, SHUT_RDWR);
+
+    close(s);
 
     exit( 0 );
 }
